060a71acec981aec4c4135353ecb2eabf7a58ca2
Madhubashana <84127691+promudith@users.noreply.github.com>
2023-06-03 10:57:39 +0530

    New Screens Added

---
+++ app/src/main/java/com/firebase/test01/RequestsGarage.kt
@@ -0,0 +1,11 @@
+package com.firebase.test01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RequestsGarage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_requests_garage)
+    }
+}
---
+++ app/src/main/java/com/firebase/test01/TaskGarage.kt
@@ -0,0 +1,11 @@
+package com.firebase.test01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TaskGarage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_garage)
+    }
+}
