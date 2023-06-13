5ac2655029b03935483ea716c5700fdb853de04b
Madhubashana <84127691+promudith@users.noreply.github.com>
2023-06-14 01:11:48 +0530

    Garage Navigation Forward Done

--- app/src/main/java/com/firebase/test01/GarageDashBoard.kt
+++ app/src/main/java/com/firebase/test01/GarageDashBoard.kt
@@ -1,11 +1,36 @@
 package com.firebase.test01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.button.MaterialButton
 
 class GarageDashBoard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_garage_dash_board)
+
+        val updateGarageProfile = findViewById<MaterialButton>(R.id.btnUpdateProfileGarage)
+        updateGarageProfile.setOnClickListener {
+            val intent = Intent(this, UpdateGarageProfile::class.java)
+            startActivity(intent)
+        }
+        val garageRequests = findViewById<MaterialButton>(R.id.btnGarageRequests)
+        garageRequests.setOnClickListener {
+            val intent = Intent(this, RequestFormGarage::class.java)
+            startActivity(intent)
+        }
+
+        val garageTask = findViewById<MaterialButton>(R.id.btnTask)
+        garageTask.setOnClickListener {
+            val intent = Intent(this, TaskGarage::class.java)
+            startActivity(intent)
+        }
+        val garagePastRecords = findViewById<MaterialButton>(R.id.btnPastRecords)
+        garagePastRecords.setOnClickListener {
+            val intent = Intent(this, PastRecordeGarage::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/firebase/test01/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/firebase/test01/ui/dashboard/DashboardFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.firebase.test01.databinding.FragmentDashboardBinding
+import com.firebase.test01.databinding.FragmentViewGarageBinding
 
 class DashboardFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentViewGarageBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class DashboardFragment : Fragment() {
         val dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentViewGarageBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
